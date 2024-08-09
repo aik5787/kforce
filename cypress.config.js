eb8573a1787d17e340cc29b38fb9785a2fde143a
Aleksandr <aik5787@gmail.com>
2024-08-09 12:24:13 -0400

    update

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: "https://www.kforce.com/",
-    viewportWidth: 1400,
-    viewportHeight: 1200,
+    viewportWidth: 1410,
+    viewportHeight: 1210,
   },
 });
